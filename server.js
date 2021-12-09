a814f7d6c8477f1eb453eed2b0a3672273e927d7
Vinay PAnicker <Vinay@192-168-68-120.tpgi.com.au>
2021-12-10 00:50:47 +1100

    production

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ io.on('connection', socket => {
 
 });
 
+if(process.env.PROD ){
+    app.use(express.static(path.join(__dirname,'./client/build')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'./client/build/index.html'));
+    });
+}
+
 server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
 
 
