4f0887288a9936300bff7bca3b532e9934bc5cf4
Vinay PAnicker <Vinay@192-168-68-120.tpgi.com.au>
2021-12-10 08:18:08 +1100

    ssl

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require("express");
-const http = require("http");
+const https = require("https");
 const app = express();
-const server = http.createServer(app);
+const fs = require("fs");
+const path = require("path");
+const server = https.createServer({key:fs.readFileSync(path.join(__dirname,'cert','key.pem')),cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))},app);
 const socket = require("socket.io");
 const io = socket(server);
-const path = require("path");
+
 
 const users = {};
 
